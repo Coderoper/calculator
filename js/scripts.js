4072176914cd026c7c942712148674cfc06b7d45
Alex Noble and Austin Roberts <alex@>
2018-03-19 11:49:58 -0700

    add formulas

---
+++ js/scripts.js
@@ -0,0 +1,29 @@
+/*var add = function(number1, number2) {
+  return number1 + number2;
+};
+var subtract = function(number1, number2){
+  return number1-number2;
+};
+var multiply = function(number1, number2){
+  return number1*number2;
+}
+var divide = function(number1, number2){
+  return number1/number2;
+}
+var number1 = parseInt(prompt("Enter a number:"));
+var number2 = parseInt(prompt("Enter another number"));
+var result = divide(number1, number2);
+alert(result);
+var height = parseFloat(prompt("what is your height in meters"));
+var weight = parseFloat(prompt("What is your weight in Kg"));
+var BMI = function(height, weight) {
+  return (weight/height)/height;
+};
+var result=BMI(height, weight);
+alert(result);
+var tempinC=parseFloat(prompt("What is the temperature in Celsius"));
+var convertCtoF =function(tempinC) {
+  return tempinC*1.8 + 32;
+};
+var result=convertCtoF(tempinC);
+alert(result);*/
